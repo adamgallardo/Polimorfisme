0d32c1b9efc350bbb9f66e0f4089aa7139c5d711
adamgallardo <adam.gallardo@estudiantat.upc.edu>
2022-09-25 13:35:27 +0200

    Main acabat i corregit

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,16 @@ public class Main {
     {
         Figura[] figura = new Figura[4];
 
-        Figura[0] = new Cercle(6);
-        Figura[1] = new Quadrat(8);
-        Figura[2] = new Rectangle(6,8);
-        Figura[3] = new Triangle(5,5);
+        figura[0] = new Cercle(6);
+        figura[1] = new Quadrat(8);
+        figura[2] = new Rectangle(6,8);
+        figura[3] = new Triangle(5,5);
 
-        double areaTotal;
+        double areaTotal=0;
 
         for (int i=0; i<4; i++){
             areaTotal = figura[i].area() + areaTotal;
     }
-        System.out.printIn("L'area total es " + areaTotal)
+        System.out.println("L'area total es " + areaTotal);
     }
     }
-
-}
