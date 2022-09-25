ae0ecf353af6f5d8783fbabd35b85a17dc9e2867
adamgallardo <adam.gallardo@estudiantat.upc.edu>
2022-09-25 13:24:40 +0200

    Main acabat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Figura[] figura = new Figura[4];
+
+        Figura[0] = new Cercle(6);
+        Figura[1] = new Quadrat(8);
+        Figura[2] = new Rectangle(6,8);
+        Figura[3] = new Triangle(5,5);
+
+        double areaTotal;
+
+        for (int i=0; i<4; i++){
+            areaTotal = figura[i].area() + areaTotal;
     }
+        System.out.printIn("L'area total es " + areaTotal)
+    }
+    }
+
 }
